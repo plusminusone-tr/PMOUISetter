3d58a8e73554a9e68ef8353312218cf6cf0e7455
Oğuzhan Karakuş <oguzhan@plusminusone.co>
2022-02-03 09:40:02 +0300

    add xcframework

---
+++ PMOUISetter.xcframework/ios-arm64_x86_64-simulator/PMOUISetter.framework/Headers/PMOUISetter.h
@@ -0,0 +1,18 @@
+//
+//  PMOUISetter.h
+//  PMOUISetter
+//
+//  Created by Oğuzhan Karakuş on 2.02.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PMOUISetter.
+FOUNDATION_EXPORT double PMOUISetterVersionNumber;
+
+//! Project version string for PMOUISetter.
+FOUNDATION_EXPORT const unsigned char PMOUISetterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PMOUISetter/PublicHeader.h>
+
+
